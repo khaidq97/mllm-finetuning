14709f2764e1cc997359e5c6daace79b11fb0a55
leo <leo@tigereye.ai>
2025-12-29 02:20:58 +0000

    update

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,4 +2,6 @@
 export CUDA_VISIBLE_DEVICES=0
 export PYTHONPATH=$PYTHONPATH:${pwd}
 
-llamafactory-cli train configs/gemma3_projector_sft.yaml
+# llamafactory-cli train configs/gemma3_projector_sft.yaml
+
+llamafactory-cli train configs/gemma3_trans_projector_head_sft.yaml
---
+++ tools/test.py
@@ -0,0 +1,54 @@
+# pip install accelerate
+
+from transformers import AutoProcessor, Gemma3ForConditionalGeneration
+from PIL import Image
+import requests
+import torch
+
+# CHECKPOINT_PATH = "/home/sagemaker-user/workspace/mllm-finetuning/outputs/gemma3_projector_sft/checkpoint-800"
+CHECKPOINT_PATH = "/home/sagemaker-user/workspace/mllm-finetuning/pretrains/gemma3-4b-it"
+IMAGE_PATH = "/home/sagemaker-user/workspace/mllm-finetuning/data/images/naruto_val_000022.png"
+PROMPT = "Describe this image in detail."
+
+image = Image.open(IMAGE_PATH).convert("RGB")
+
+model = Gemma3ForConditionalGeneration.from_pretrained(
+    CHECKPOINT_PATH,
+    trust_remote_code=True,
+    dtype=torch.bfloat16,
+    device_map="auto"
+).eval()
+
+processor = AutoProcessor.from_pretrained(
+    CHECKPOINT_PATH,
+    trust_remote_code=True
+)   
+
+messages = [
+    {
+        "role": "system",
+        "content": [{"type": "text", "text": "You are a helpful assistant."}]
+    },
+    {
+        "role": "user",
+        "content": [{"type": "image", "image": image}, {"type": "text", "text": PROMPT}]
+    }
+]
+
+inputs = processor.apply_chat_template(
+    messages, add_generation_prompt=True, tokenize=True,
+    return_dict=True, return_tensors="pt"
+).to(model.device, dtype=torch.bfloat16)
+
+input_len = inputs["input_ids"].shape[-1]
+
+with torch.inference_mode():
+    generation = model.generate(**inputs, max_new_tokens=100, do_sample=False)
+    generation = generation[0][input_len:]
+
+decoded = processor.decode(generation, skip_special_tokens=True)
+print(decoded)
+
+# **Overall Impression:** The image is a close-up shot of a vibrant garden scene, 
+# focusing on a cluster of pink cosmos flowers and a busy bumblebee. 
+# It has a slightly soft, natural feel, likely captured in daylight.
